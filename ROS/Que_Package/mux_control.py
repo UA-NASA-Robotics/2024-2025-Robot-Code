e8e9cd743659626a6a31b93651b84c43ea3015e5
Piglet337 <162376649+Piglet337@users.noreply.github.com>
2024-11-08 17:30:57 +0000

    Reversed unfinished changes to mux_control

--- ROS/Que_Package/mux_control.py
+++ ROS/Que_Package/mux_control.py
@@ -9,20 +9,11 @@ class MuxControl(Node):
         super().__init__('mux_control')
 
         #create subscriber listening to output from RS_package
-        self.mux_subscriber = self.create_subscription(TwistPlus, '/rs/cmd_vel', self.callbackChangeMux, 10)
         
         #create a client to execute the switch_mux service
-        self.cli = self.create_client(switch_mux, 'switch_mux') 
-        while not self.cli.wait_for_service(timeout_sec=1.0): 
-            self.get_logger().info('Service not available, waiting...') 
-        self.req = switch_mux.Request()
 
     #create a function that makes a request to switch to the /rs/cmd_vel 
     #when a message over /rs/cmd_vel is recieved
-    def callbackChangeMux(self, topic):
-       mux_control.send_request('/rs/cmd_vel')
-        
-
 
 def main(args=None):
     rclpy.init(args=args)
