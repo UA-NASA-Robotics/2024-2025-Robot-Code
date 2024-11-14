b33d6c38174f86749bfba1ce9acb82ce92dd6c97
Elia Albaba <ea152@uakron.edu>
2024-11-14 18:47:20 -0500

    Add queue to the fifoQueue node

--- ROS/queue_package/queue_package/fifo_queue.py
+++ ROS/queue_package/queue_package/fifo_queue.py
@@ -1,15 +1,37 @@
 import rclpy
 from rclpy.node import Node
+import queue
 
 class FifoQueue(Node):
     #Initialize node and crease subscribers and clients
     def __init__(self):
         super().__init__('fifo_queue')
+        self.queue = queue.Queue()
+
+    def enqueue(self, item):
+        self.queue.put(item)
+        self.get_logger().info(f'Item {item} enqueued')
+
+    def dequeue(self):
+        if not self.queue.empty():
+            item = self.queue.get()
+            self.get_logger().info(f'Item {item} dequeued')
+            return item
+        else:
+            self.get_logger().info('Queue is empty')
+            return None
 
 def main(args=None):
     rclpy.init(args=args)
     fifo_queue = FifoQueue()
     rclpy.spin(fifo_queue)
+
+    fifo_queue.enqueue('something')
+    fifo_queue.enqueue('something else')
+
+    fifo_queue.dequeue()
+    fifo_queue.dequeue()
+
     fifo_queue.destroy_node()
     rclpy.shutdown()
 
