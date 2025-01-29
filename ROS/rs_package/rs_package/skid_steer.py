ebcdbd2e8d09890fdc7b7da115968c15d255dfac
Jon <jar335@uakron.edu>
2025-01-28 22:42:43 -0500

    Fixed logic to allow buttons to be mapped to axes

--- ROS/rs_package/rs_package/skid_steer.py
+++ ROS/rs_package/rs_package/skid_steer.py
@@ -18,29 +18,29 @@ class ControllerInterpreter(Node):
         # For example, axes_map[1] represents the Left Stick Y
         # If 'left_drive' is in axes_map[1], then Left Stick Y controls the left drive
         axes_map = [
-            '',                 # Left Stick X
-            'left_drive',       # Left Stick Y
-            '',                 # Right Stick X
-            'right_drive',      # Right Stick Y
-            '',                 # Left Trigger (L2)
-            '',                 # Right Trigger (R2)
+            '',                     # Left Stick X
+            'left_drive',           # Left Stick Y
+            '',                     # Right Stick X
+            'right_drive',          # Right Stick Y
+            'actuator_arm_down',    # Left Trigger (L2)
+            'actuator_pitch_down',  # Right Trigger (R2)
         ]
         button_map = [
-            'autonomy_enable',  # A (X)
-            '',                 # B (◯)
-            '',                 # X (□)
-            '',                 # Y (△)
-            '',                 # Menu
-            '',                 # Home
-            '',                 # Start
-            '',                 # Left Stick
-            '',                 # Right Stick
-            '',                 # Left Bumper (L1)
-            '',                 # Right Bumper (R1)
-            '',                 # D-Pad Up
-            '',                 # D-Pad Down
-            '',                 # D-Pad Left
-            '',                 # D-Pad Right
+            'autonomy_enable',      # A (X)
+            '',                     # B (◯)
+            '',                     # X (□)
+            '',                     # Y (△)
+            '',                     # Menu
+            '',                     # Home
+            '',                     # Start
+            '',                     # Left Stick
+            '',                     # Right Stick
+            'actuator_arm_up',      # Left Bumper (L1)
+            'actuator_pitch_up',    # Right Bumper (R1)
+            '',                     # D-Pad Up
+            '',                     # D-Pad Down
+            '',                     # D-Pad Left
+            '',                     # D-Pad Right
         ]
 
         # Don't send many null packets (reduce bandwidth usage)
@@ -72,7 +72,7 @@ class ControllerInterpreter(Node):
 
         # Set both parts of TwistPlus message
         self.set_twist(msg, output, axes_parameters, wheel_radius, wheel_separation)
-        self.set_buttons(msg, output, button_parameters)
+        self.set_buttons(msg, output, button_parameters, axes_parameters)
 
         # If the output packet is all 0's
         if self.test_null(output):
@@ -99,8 +99,10 @@ class ControllerInterpreter(Node):
         right_percent = input.axes[axes.index('right_drive')]
         
         # Formula was derrived from the opposite (take linear and angular to left and right percent)
-        output.linear.x = (left_percent + right_percent) * wheel_radius / 2
-        output.angular.z = 2 * (output.linear.x - left_percent * wheel_radius) / wheel_separation
+        if abs(left_percent) >= 0.05 or abs(right_percent) >= 0.05:
+            setattr(output.buttons, 'button_weels_ismoving', True)
+            output.linear.x = (left_percent + right_percent) * wheel_radius / 2
+            output.angular.z = 2 * (output.linear.x - left_percent * wheel_radius) / wheel_separation
 
         # Set everything else to 0's
         output.linear.y, output.linear.z = 0.0, 0.0
@@ -111,21 +113,24 @@ class ControllerInterpreter(Node):
 
     # Prepares Button part of TwistPlus
     def set_buttons(self, input: Joy, output: TwistPlus, 
-                    buttons: list[str]) -> TwistPlus:
-        # TODO: implement a better way than setting buttons one at a time
-        output.buttons.autonomy_enable = input.buttons[buttons.index('autonomy_enable')] == 1
+                    buttons: list[str], axes: list[str]) -> TwistPlus:
+        for item in [item for item in buttons + axes if item != '' if 'button_' + item in dir(output.buttons)]:
+            full_name = 'button_' + item
+            if item in buttons:
+                setattr(output.buttons, full_name,
+                        input.buttons[buttons.index(item)] == 1)
+            elif item in axes:
+                setattr(output.buttons, full_name,
+                        abs(input.axes[axes.index(item)]) >= 0.5)
 
         return output
     
     # Goes through every element in the Joy topic (other than timing)
     # Returns False if something is not 0, otherwise returns true
     def test_null(self, msg: TwistPlus) -> bool:
-        for i in [msg.linear, msg.angular]:
-            for j in [i.x, i.y, i.z]:
-                if abs(j) >= 0.05:
-                    return False
-        if msg.buttons.autonomy_enable:
-            return False
+        for item in [item[1:] for item in msg.buttons.__slots__ if 'button' in item]:
+            if getattr(msg.buttons, item):
+                return False
         return True
 
 def main(args=None):
